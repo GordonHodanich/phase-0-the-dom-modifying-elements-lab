559198e66e43e7eb9578aa2946a06ec17154b371
Gordon Hodanich <ghodanich@gmail.com>
2021-05-27 15:35:49 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+var newHeader = document.createElement("h1")
+document.body.appendChild(newHeader).setAttribute("id", 'victory')
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
